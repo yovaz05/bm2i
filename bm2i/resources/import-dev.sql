84d7a9a402e408b8e946fab340e8131cddef70a8
richardmijo@gmail.com <richardmijo@gmail.com@cacc8a0a-26b5-0056-62e9-0213ccd7e276>
2012-05-23 23:54:08 +0000

    registro de usuario pricipal del sistema ingreso de archivo .properties para crear idiomas

--- bm2i/resources/import-dev.sql
+++ bm2i/resources/import-dev.sql
@@ -1,1 +1,4 @@
 -- SQL statements which are executed at application startup if hibernate.hbm2ddl.auto is 'create' or 'create-drop'
+--la creacion del usuario principal
+insert into resident values("N";1;"richardmijo@hotmail.com";"2012-09-22";"RICHARD FRANCISCO ARMIJOS MERINO";"1104111271";"CEDULA";"ARMIJOR MERINO";"2012-09-22";"SOLTERO";"MASCULINO";FALSE;"RICHARD FRANCISCO";"";"";"";"")
+insert into user values (1;"2012-09-22";TRUE;"mijo";"pris1";1)
