dc618e3ccf357dbb165c904466cd10f76eb1f16b
albert.reyess90@gmail.com <albert.reyess90@gmail.com@cacc8a0a-26b5-0056-62e9-0213ccd7e276>
2012-12-11 05:05:19 +0000

    mapa

---
+++ 4life/src/Datos/FacturaHBM.java
@@ -0,0 +1,64 @@
+/*
+ * AgenciaHBM.java
+ *
+ * Created on 29 de septiembre de 2006, 13:18
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package Datos;
+
+import beans.Factura;
+import hibernate.HibernateDAO;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+public class FacturaHBM extends HibernateDAO{
+    
+    /** Creates a new instance of AgenciaHBM */
+    public FacturaHBM() {
+    }
+    public List lista(){
+        Criteria crit = session.createCriteria(Factura.class);     
+        crit.addOrder(Order.asc("nombre"));
+        return crit.list();
+    }
+   
+    public Factura buscar(String nombre) {
+        Criteria crit = session.createCriteria(Factura.class);
+        crit.add(Restrictions.eq("nombre",nombre));
+            if(crit.list().size()>1)
+            return null;
+        else
+            return (Factura)crit.uniqueResult();
+    }
+    
+    public Factura buscarCl(String nombre) {
+        Criteria crit = session.createCriteria(Factura.class);
+        crit.add(Restrictions.eq("codigo",nombre));
+            if(crit.list().size()>1)
+            return null;
+        else
+            return (Factura)crit.uniqueResult();
+    }
+    
+    
+    
+    
+    public Factura busca(String cedula){
+        Criteria crit = session.createCriteria(Factura.class);
+        crit.add(Restrictions.eq("codigo",cedula));
+        if(crit.list().size()>1){
+            System.out.println("no hay ususario");
+            return null;
+        }
+        else{
+         //   System.out.println("si hay");
+            return (Factura)crit.uniqueResult();
+        }
+    }
+    
+}
